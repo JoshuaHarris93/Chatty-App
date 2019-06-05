5f3fd059b6cad2df01de0b20632e81481de721f9
JoshuaHarris93 <josh@emptychairproductions.com>
2019-06-05 23:29:43 +0000

    change usernames

--- chatty_server/chattyServer.js
+++ chatty_server/chattyServer.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const SocketServer = require("ws").Server;
-const uuid = require("uuid");
+const uuidv4 = require("uuid/v4");
 
 // Set the port to 3001
 const PORT = 3001;
@@ -16,41 +16,29 @@ const server = express()
 // Create the WebSockets server
 const wss = new SocketServer({ server });
 
-wss.on("connection", ws => {
-    
-    wss.broadcast = function broadcast(message) {
-        wss.clients.forEach(function each(client) {
-          if (client.readyState === SocketServer.OPEN) {
-            client.send(message);
-          }
-        });
-      };
+// wss.on("connection", ws => {
+  wss.broadcast = function broadcast(message) {
+    wss.clients.forEach(function each(client) {
+      client.send(message);
+    });
+  };
+
+  wss.on("connection", function connection(ws) {
+      console.log("Client connected");
       
-      wss.on('connection', function connection(ws) {
-        ws.on('message', function incoming(message) {
-          // Broadcast to everyone else.
-          const id = JSON.parse(message).id;
-        const username = JSON.parse(message).username;
-        const content = JSON.parse(message).content;
-          
-        wss.clients.forEach(function each(client) {
-            if (client !== ws && client.readyState === SocketServer.OPEN) {
-              client.send('User', username , 'said', content, 'id', id);
-            }
-          });
-        });
-      });    
+    ws.on("message", function incoming(message) {
+    const messageObject = JSON.parse(message);
+    messageObject.id = uuidv4();
+    if (messageObject.type === "outgoingMessage") {
+      messageObject.type = "incomingMessage";
+    }
+    const username = messageObject.username;
+    const content = messageObject.content;
 
+    console.log("User", username, "said", content, "id", messageObject.id);
+    wss.broadcast(JSON.stringify(messageObject));
+  });
 
-//         ws.on('message', function incoming(message) {
-//         const id = JSON.parse(message).id;
-//         const username = JSON.parse(message).username;
-//         const content = JSON.parse(message).content;
-
-//         console.log('User', username , 'said', content, 'id', id);
-//       });
-//       ws.send("message from server")
-//   console.log("Client connected");
 
   // Set up a callback for when a client closes the socket. This usually means they closed their browser.
   ws.on("close", () => console.log("Client disconnected"));
